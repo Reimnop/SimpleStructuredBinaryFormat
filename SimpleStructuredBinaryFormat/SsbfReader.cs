c9e8caf34f81b0348248eaf8ad8e723ca5171c86
Reimnop <rayngu2005@gmail.com>
2026-06-15 12:56:03 +0700

    Use System.IO BrotliStream instead...

--- SimpleStructuredBinaryFormat/SsbfReader.cs
+++ SimpleStructuredBinaryFormat/SsbfReader.cs
@@ -3,7 +3,6 @@ using System.IO.Compression;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Text;
-using BrotliStream = BrotliSharpLib.BrotliStream;
 
 namespace SimpleStructuredBinaryFormat;
 
